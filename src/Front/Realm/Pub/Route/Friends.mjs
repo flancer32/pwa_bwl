c7486f7471e741d39b568c6d763b21e0bdbaad4e
Alex Gusev <alex@flancer64.com>
2021-05-13 12:21:18 +0300

    MF-87 Dialog to select type of new relation (new or friend)

--- src/Front/Realm/Pub/Route/Friends.mjs
+++ src/Front/Realm/Pub/Route/Friends.mjs
@@ -21,11 +21,10 @@ const template = `
             row-key="${FRIEND_ID}"
     >
     </q-table>
-    <edit-group
-        :display="dialogDisplay"
-        @onHide="dialogDisplay=false"
-        v-model="selectedItem"
-    ></edit-group>
+    <dialog-add
+        :display="dialogAddDisplay"
+        @onHide="dialogAddDisplay=false"
+    ></dialog-add>
 </div>
 `;
 
@@ -47,6 +46,8 @@ function Factory(spec) {
     const {ref} = spec[DEF.MOD_VUE.DI_VUE];    // destructuring instance singleton
     /** @type {typeof Fl32_Bwl_Front_Layout_TopActions.IComponent} */
     const topActions = spec[DEF.DI_TOP_ACTIONS]; // vue comp tmpl
+    /** @type {Fl32_Bwl_Front_Realm_Pub_Widget_Friends_Dialog_Add} */
+    const dialogAdd = spec['Fl32_Bwl_Front_Realm_Pub_Widget_Friends_Dialog_Add$']; // vue comp tmpl
     /** @type {Fl32_Bwl_Front_Widget_Edit_Group} */
     const editGroup = spec['Fl32_Bwl_Front_Widget_Edit_Group$']; // vue comp tmpl
     /** @type {typeof Fl32_Bwl_Front_Layout_TopActions.Item} */
@@ -66,11 +67,11 @@ function Factory(spec) {
     return {
         name: NS,
         template,
-        components: {editGroup},
+        components: {editGroup, dialogAdd},
         data() {
             return {
                 rows: [],
-                dialogDisplay: false,
+                dialogAddDisplay: false,
                 selectedItem: null,
             };
         },
@@ -81,7 +82,7 @@ function Factory(spec) {
                 for (const one of this.rows) {
                     if (one.friendId === friendId) this.selectedItem = one;
                 }
-                this.dialogDisplay = true;
+                this.dialogAddDisplay = true;
             }
         },
         async mounted() {
@@ -90,9 +91,9 @@ function Factory(spec) {
 
             // DEFINE INNER FUNCTIONS
             /**
-             * Reset Top Actions on component re-mount.
+             * Reset 'Top Actions' commands on component re-mount.
              */
-            function addTopActions() {
+            function resetTopActions() {
                 const actAdd = new Action();
                 actAdd.icon = 'add';
                 actAdd.action = async function () {
@@ -102,38 +103,39 @@ function Factory(spec) {
                      * @returns {Promise<void>}
                      */
                     async function addNew() {
-                        try {
-                            // get referral link with limited lifetime
-                            const req = new ReqRefLinkCreate();
-                            /** @type {Fl32_Teq_User_Shared_Service_Route_RefLink_Create_Response} */
-                            const res = await gateRefLinkCreate(req);
-                            const code = res.link.refCode;
-                            // compose URL to sign up
-                            const host = `https://${config.urlBase}`;
-                            const realm = `/${DEF.REALM_SIGN}/#`;
-                            const route = DEF.REALM_SIGN_ROUTE_UP.replace(':refCode?', code);
-                            const url = `${host}${realm}${route}`;
-                            // open sharing options or print out sign up link to console
-                            if (self.navigator.share) {
-                                // smartphone mode
-                                const data = {
-                                    title: 'Bruderschaft Weight Loss',
-                                    text: i18n.t('route.friends.share.welcome'),
-                                    url,
-                                };
-                                await self.navigator.share(data);
-                            } else {
-                                // browser mode
-                                console.log(`sign up url: ${url}`);
-                            }
-
-                        } catch (err) {
-                            console.log(`error: ${err}`);
-                        }
+                        me.dialogAddDisplay = true; // display 'Add' dialog to select type of user: downline or friend
+                        // try {
+                        //     // get referral link with limited lifetime
+                        //     const req = new ReqRefLinkCreate();
+                        //     /** @type {Fl32_Teq_User_Shared_Service_Route_RefLink_Create_Response} */
+                        //     const res = await gateRefLinkCreate(req);
+                        //     const code = res.link.refCode;
+                        //     // compose URL to sign up
+                        //     const host = `https://${config.urlBase}`;
+                        //     const realm = `/${DEF.REALM_SIGN}/#`;
+                        //     const route = DEF.REALM_SIGN_ROUTE_UP.replace(':refCode?', code);
+                        //     const url = `${host}${realm}${route}`;
+                        //     // open sharing options or print out sign up link to console
+                        //     if (self.navigator.share) {
+                        //         // smartphone mode
+                        //         const data = {
+                        //             title: 'Bruderschaft Weight Loss',
+                        //             text: i18n.t('route.friends.share.welcome'),
+                        //             url,
+                        //         };
+                        //         await self.navigator.share(data);
+                        //     } else {
+                        //         // browser mode
+                        //         console.log(`sign up url: ${url}`);
+                        //     }
+                        //
+                        // } catch (err) {
+                        //     console.log(`error: ${err}`);
+                        // }
                     }
 
                     async function editSelected() {
-                        me.dialogDisplay = true;
+                        me.dialogAddDisplay = true;
                     }
 
                     // MAIN FUNCTIONALITY
@@ -148,7 +150,7 @@ function Factory(spec) {
 
             // MAIN FUNCTIONALITY
             if (await session.checkUserAuthenticated(this.$router)) {
-                addTopActions();
+                resetTopActions();
                 this.rows = [];
             }
         },
---
+++ src/Front/Realm/Pub/Widget/Friends/Dialog/Add.mjs
@@ -0,0 +1,100 @@
+/**
+ * Dialog widget to select new relation type: friend (already registered user) or new user in downline.
+ *
+ * @namespace Fl32_Bwl_Front_Realm_Pub_Widget_Friends_Dialog_Add
+ */
+// MODULE'S VARS
+const NS = 'Fl32_Bwl_Front_Realm_Pub_Widget_Friends_Dialog_Add';
+const EVT_HIDE = 'onHide';
+const EVT_SUBMIT = 'onSubmit';
+const TYPE_DOWN = 'downline';
+const TYPE_FRIEND = 'friend';
+
+// MODULE'S FUNCTIONS
+/**
+ * Factory to create template for new Vue component instances.
+ *
+ * @memberOf Fl32_Bwl_Front_Realm_Pub_Widget_Friends_Dialog_Add
+ * @returns {Fl32_Bwl_Front_Realm_Pub_Widget_Friends_Dialog_Add.vueCompTmpl}
+ */
+function Factory(spec) {
+    // EXTRACT DEPS
+    /** @type {Fl32_Bwl_Defaults} */
+    const DEF = spec['Fl32_Bwl_Defaults$'];
+    /** @type {TeqFw_Di_Container} */
+    const container = spec[DEF.MOD_CORE.DI_CONTAINER]; // named singleton
+    const i18next = spec[DEF.MOD_CORE.DI_I18N]; // named singleton
+    const {ref} = spec[DEF.MOD_VUE.DI_VUE];    // named singleton destructuring
+
+    // DEFINE WORKING VARS
+    const template = `
+<q-dialog :model-value="display" @hide="$emit('${EVT_HIDE}')">
+    <q-card style="min-width: 350px">
+        <q-card-section class="t-grid gutter-xs align-items-center"
+                        style="grid-template-: auto auto; justify-items: left;">
+                        
+                <q-radio
+                        :label="$t('wg.friends.dialog.add.downline')"
+                        v-model="fldType"
+                        val="${TYPE_DOWN}"
+                ></q-radio>
+                <q-radio
+                        :label="$t('wg.friends.dialog.add.friend')"
+                        v-model="fldType"
+                        val="${TYPE_FRIEND}"
+                ></q-radio>
+
+        </q-card-section>
+
+        <q-card-actions align="center" class="text-primary">
+            <q-btn flat :label="$t('btn.cancel')" v-close-popup></q-btn>
+            <q-btn flat :label="$t('btn.ok')" v-close-popup v-on:click="submit"></q-btn>
+        </q-card-actions>
+
+    </q-card>
+</q-dialog>
+`;
+
+    // COMPOSE RESULT
+    /**
+     * Template to create new component instances using Vue.
+     *
+     * @const {Object} vueCompTmpl
+     * @memberOf Fl32_Bwl_Front_Realm_Pub_Widget_Friends_Dialog_Add
+     */
+    return {
+        /* Misc: https://v3.vuejs.org/api/options-misc.html */
+        name: NS,
+        template,
+        data() {
+            return {
+                fldType: TYPE_DOWN
+            };
+        },
+        props: {
+            display: Boolean, // control hide/display the widget from parent
+        },
+        methods: {
+            /**
+             * Pass selected values to the parent.
+             */
+            submit() {
+                this.$emit(EVT_SUBMIT, this.groupName, this.owner);
+            }
+        },
+        watch: {
+            display(current) {
+
+            },
+
+        },
+        emits: [EVT_HIDE, EVT_SUBMIT],
+        mounted() {
+            // debugger
+        },
+    };
+}
+
+// MODULE'S EXPORT
+Object.defineProperty(Factory, 'name', {value: `${NS}.${Factory.name}`});
+export default Factory;
