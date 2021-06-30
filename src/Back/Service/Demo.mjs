13e9d2f8a8d058a0e6d08d12423df5581e3f5be5
Alex Gusev <alex@flancer64.com>
2021-06-30 15:38:30 +0300

    WF-457 Create @teqfw/web plugin

---
+++ src/Back/Service/Demo.mjs
@@ -0,0 +1,49 @@
+/**
+ * Demo service to develop HTTP Service Handler.
+ *
+ * @namespace Fl32_Bwl_Back_Service_Demo
+ */
+// MODULE'S IMPORT
+import {constants as H2} from 'http2';
+// MODULE'S VARS
+const NS = 'Fl32_Bwl_Back_Service_Demo';
+
+/**
+ * @implements TeqFw_Web_Back_Api_Service_IFactory
+ */
+export default class Fl32_Bwl_Back_Service_Demo {
+    constructor(spec) {
+        // EXTRACT DEPS
+        /** @type {Fl32_Bwl_Shared_Service_Route_Demo.Factory} */
+        const fReqRes = spec['Fl32_Bwl_Shared_Service_Route_Demo#Factory$'];
+
+        // DEFINE INSTANCE METHODS
+        this.getDtoFactory = function () {
+            return fReqRes;
+        }
+
+        this.getRoute = function () {
+            return '/demo';
+        }
+
+        this.getService = function () {
+            // DEFINE INNER FUNCTIONS
+            /**
+             *
+             * @param {TeqFw_Web_Back_Api_Service_IContext} context
+             */
+            function service(context) {
+                /** @type {TeqFw_Web_Back_Api_Service_IContext} : IDEA suggestions failure */
+                const ctx = context;
+                const out = ctx.getOutData();
+                out.success = true;
+                ctx.setOutHeader(H2.HTTP2_HEADER_ACCEPT_CHARSET, 'utf8');
+            }
+
+            // MAIN FUNCTIONALITY
+            Object.defineProperty(service, 'name', {value: `${NS}.${service.name}`});
+            return service;
+        }
+    }
+
+}
---
+++ src/Shared/Service/Route/Demo.mjs
@@ -0,0 +1,62 @@
+/**
+ * Request and response for demo service to develop new format services.
+ *
+ * @namespace Fl32_Bwl_Shared_Service_Route_Demo
+ */
+// MODULE'S VARS
+const NS = 'Fl32_Bwl_Shared_Service_Route_Demo';
+
+// MODULE'S CLASSES
+/**
+ * @memberOf Fl32_Bwl_Shared_Service_Route_Demo
+ */
+class Request {
+    /** @type {string} */
+    lang;
+}
+
+/**
+ * @memberOf Fl32_Bwl_Shared_Service_Route_Demo
+ */
+class Response {
+    /** @type {boolean} */
+    success;
+}
+
+/**
+ * Factory to create new DTOs.
+ * @memberOf Fl32_Bwl_Shared_Service_Route_Demo
+ */
+class Factory {
+    constructor() {
+        /**
+         * @param {Request|Object|null} data
+         * @return {Fl32_Bwl_Shared_Service_Route_Demo.Request}
+         */
+        this.createReq = function (data = null) {
+            const res = new Request();
+            res.lang = data?.lang;
+            return res;
+        }
+
+        /**
+         * @param {Response|Object|null} data
+         * @return {Fl32_Bwl_Shared_Service_Route_Demo.Response}
+         */
+        this.createRes = function (data = null) {
+            const res = new Response();
+            res.success = data?.success || false;
+            return res;
+        }
+    }
+}
+
+// MODULE'S EXPORT
+Object.defineProperty(Factory, 'name', {value: `${NS}.${Factory.constructor.name}`});
+Object.defineProperty(Request, 'name', {value: `${NS}.${Request.constructor.name}`});
+Object.defineProperty(Response, 'name', {value: `${NS}.${Response.constructor.name}`});
+export {
+    Factory,
+    Request,
+    Response,
+};
