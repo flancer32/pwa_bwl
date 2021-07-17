5115222db80883012f8c43270c6146698c8be983
Alex Gusev <alex@flancer64.com>
2021-07-17 12:06:22 +0300

    WF-468 Habr: services

--- src/Shared/Service/Route/Friend/Link/Add.mjs
+++ src/Shared/Service/Route/Friend/Link/Add.mjs
@@ -30,7 +30,7 @@ class Response {
 
 /**
  * Factory to create new DTOs and get route address.
- * @implements TeqFw_Web_Back_Api_Service_Factory_IRoute
+ * @implements TeqFw_Web_Back_Api_Service_IRoute
  * @memberOf Fl32_Bwl_Shared_Service_Route_Friend_Link_Add
  */
 class Factory {
--- src/Shared/Service/Route/Sign/Up.mjs
+++ src/Shared/Service/Route/Sign/Up.mjs
@@ -39,7 +39,7 @@ class Response {
 
 /**
  * Factory to create new DTOs and get route address.
- * @implements TeqFw_Web_Back_Api_Service_Factory_IRoute
+ * @implements TeqFw_Web_Back_Api_Service_IRoute
  * @memberOf Fl32_Bwl_Shared_Service_Route_Sign_Up
  */
 class Factory {
